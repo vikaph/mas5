7a3b50f250d67dd5f2a57f5f17cf91a94f675a72
357-22 <vika.p.2006@bk.ru>
2024-10-26 10:11:26 +0300

    Готовое решение

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите слова через пробел:")
+    val words = readLine()?.split(" ") ?: emptyList()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val groupedWords = groupAnagrams(words)
+
+    for (group in groupedWords) {
+        println(group.joinToString(", "))
     }
+}
+// Функция для группировки анаграмм
+fun groupAnagrams(words: List<String>): List<List<String>> {
+    val anagramMap = mutableMapOf<String, MutableList<String>>()
+
+    for (word in words) {
+        // Сортируем буквы в слове, чтобы получить ключ для анаграммы
+        val sortedWord = word.toCharArray().sorted().joinToString("")
+
+        // Добавляем слово в соответствующую группу
+        anagramMap.computeIfAbsent(sortedWord) { mutableListOf() }.add(word)
+    }
+    return anagramMap.values.toList()
 }
